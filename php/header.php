a4cb302cd589e83c233a5c8d943f1e4fbd849630
Ben McCarty <benmccarty91@gmail.com>
2018-09-12 12:12:32 -0600

    fixing broken image links

--- php/header.php
+++ php/header.php
@@ -25,7 +25,7 @@
 			</div>
 			<div id="spacer">
 				<a href="https://www.themoviedb.org/" target="_blank">
-					<img src="https://www.themoviedb.org/static_cache/v4/logos/408x161-powered-by-rectangle-blue-10d3d41d2a0af9ebcb85f7fb62ffb6671c15ae8ea9bc82a2c6941f223143409e.png" />
+					<img src="https://www.themoviedb.org/assets/1/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg" />
 				</a>
 			</div>
 			<nav>
