199ad9bb1dc0a55dd68a42c489bc4728f9e0e00d
Ben McCarty <benmccarty91@gmail.com>
2019-04-24 21:48:46 -0600

    adding fields to the question model

--- php/androidQuestionHandler.php
+++ php/androidQuestionHandler.php
@@ -1,10 +1,13 @@
 <?php
 require_once "../php/Dao.php";
-
 $dao = new Dao();
+
+$gameId = $_GET['gameId'];
 
 $randomQuestion = $dao->getRandomQuestion();
 $toReturn = Array();
+$toReturn['gameId'] = $gameId;
+$toReturn['questionId'] = $randomQuestion[0]['question_id'];
 $toReturn['actorName'] = $randomQuestion[0]['query'];
 $toReturn['imgURL'] = $randomQuestion[0]['image_path'];
 echo (json_encode($toReturn));
